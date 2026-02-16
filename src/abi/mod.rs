b900097208605da98495e0f87ff5f4ceb7f0901e
dlr-a <dlra.htpgl@gmail.com>
2026-02-16 22:45:06 +0300

    feat(abi): add token contract events

--- src/abi/mod.rs
+++ src/abi/mod.rs
@@ -2,8 +2,10 @@ mod erc20;
 mod pool;
 mod pool_configurator;
 mod protocol_data_provider;
+mod token;
 
 pub use erc20::*;
 pub use pool::*;
 pub use pool_configurator::*;
 pub use protocol_data_provider::*;
+pub use token::*;
---
+++ src/abi/token.rs
@@ -0,0 +1,26 @@
+use alloy::sol;
+
+sol! {
+    event Mint(
+        address indexed caller,
+        address indexed onBehalfOf,
+        uint256 value,
+        uint256 balanceIncrease,
+        uint256 index
+    );
+
+    event Burn(
+        address indexed from,
+        address indexed target,
+        uint256 value,
+        uint256 balanceIncrease,
+        uint256 index
+    );
+
+    event BalanceTransfer(
+        address indexed from,
+        address indexed to,
+        uint256 value,
+        uint256 index
+    );
+}
